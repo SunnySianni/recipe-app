73417ff6bd9e1e577e3b50b9723e7285c07c20a7
SunnySianni <sstri0041@launchpadphilly.org>
2025-01-10 12:22:08 -0500

    "committed"

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ sequelize.sync()
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}/recipe`);
 });
