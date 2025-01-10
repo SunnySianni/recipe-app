05fb033fecd3fd5412359a8c5ec350bc48e7a968
SunnySianni <sstri0041@launchpadphilly.org>
2025-01-10 11:09:33 -0500

    not running yet

--- models/recipe.js
+++ models/recipe.js
@@ -1,19 +1,28 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/dbConnection.js';
+import sequelize from '../config/dbconnection.js';
 
-const Recipe = sequelize.define('Recipe', {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Recipe = sequelize.define(
+  'Recipe',
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  ingredients: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-  instructions: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+  {
+    tableName: 'recipeapp', // Specify the table name
+    timestamps: true, // Enable createdAt and updatedAt fields
+    createdAt: 'created_at',  // Specify the column name for createdAt
+    updatedAt: 'updated_at',  // Specify the column name for updatedAt
+  }
+);
 
 export default Recipe;
--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3007;
+
+
 
 // Middleware
 app.use(express.json());
